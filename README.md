432746202e0452a9e22382ea8d6293c7c70fea5a
Fukuma Shinichi <fukuma@caneretina.local>
2014-05-28 17:04:42 +0900

    a-map

---
+++ README.md
@@ -0,0 +1,4 @@
+jaxa-graph
+==========
+
+jaxa-graph
---
+++ js/main.js
@@ -0,0 +1,27 @@
+var data = {};
+var selectedFile = [];
+var selectedRows = [];
+var tabs;
+var data_dir = "";
+$(document).ready(function(){
+    getFeed();
+});
+
+function init(){
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: 'YOUR_CLIENT_ID'
+    });
+    feed.run();
+}
+
+function getFeed(){
+	$.ajax('https://api.instagram.com/v1/users/self/media/recent?access_token=1449629.114ad56.71120074c8bc4e2e952f18890106423d', {
+		type: 'GET',
+		dataType: 'json',
+		success: function(_json) {
+			console.log(_json);
+		}
+	});
+}
