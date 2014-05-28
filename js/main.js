802b6590e02cc126a650937e5fec99d431836737
Fukuma Shinichi <fukuma@caneretina.local>
2014-05-28 17:56:05 +0900

    tag only

--- js/main.js
+++ js/main.js
@@ -8,17 +8,24 @@ $(document).ready(function(){
 });
 
 function init(){
-    var feed = new Instafeed({
-        get: 'user',
-        userId: 3100907,
-        accessToken: '1449629.114ad56.71120074c8bc4e2e952f18890106423d',
-        clientId: '114ad5620479499b9302a2c2df8e6f7e',
-        limit: 60,
-		filter: function(image) {
-			return image.tags.indexOf('あーマップ') >= 0;
-		}
-    });
-    feed.run();
+  //   var feed = new Instafeed({
+  //       get: 'user',
+  //       userId: 3100907,
+  //       accessToken: '1449629.114ad56.71120074c8bc4e2e952f18890106423d',
+  //       clientId: '114ad5620479499b9302a2c2df8e6f7e',
+  //       limit: 60,
+		// filter: function(image) {
+		// 	return image.tags.indexOf('あーマップ') >= 0;
+		// }
+  //   });
+	var feed = new Instafeed({
+		get: 'tagged',
+		tagName: 'あーマップ',
+		accessToken: '1449629.114ad56.71120074c8bc4e2e952f18890106423d',
+		clientId: '114ad5620479499b9302a2c2df8e6f7e',
+		limit: 60
+	});
+	feed.run();
 }
 
 function getFeed(){
