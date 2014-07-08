7161cacafe404abc8f9a1c59248a7dbcbc6be6a9
Fukuma Shinichi <fukuma.s@gmail.com>
2014-07-08 18:55:12 +0900

    access

--- a-map/js/main.js
+++ a-map/js/main.js
@@ -7,38 +7,25 @@ var data_dir = "";
 var ACCESS_TOKEN = "";
 $(document).ready(function(){
 	// var params = get_url_vars();
-	// if (params['code']){
-	// 	getAccessToken(params['code']);
-	// }
+	if (location.hash){
+		var hash = location.hash;
+		if (hash.indexOf('#access_token=') != -1){
+			ACCESS_TOKEN = hash.replace('#access_token=','');
+			alert(ACCESS_TOKEN);
+			getUserInfo();
+		}
+	}
     init();
 });
 
 function init(){
 }
 
-function get_url_vars()
-{
-  var vars = new Object, params;
-  var temp_params = window.location.search.substring(1).split('&');
-  for(var i = 0; i <temp_params.length; i++) {
-    params = temp_params[i].split('=');
-    vars[params[0]] = params[1];
-  }
-  return vars;
-}
+function getUserInfo(){
 
-function getAccessToken(_code){
-
-	$.ajax('https://api.instagram.com/oauth/access_token', {
+	$.ajax('https://api.instagram.com/v1/users/self', {
 		type: 'POST',
 		dataType: 'json',
-		data: {
-			'client_id':'114ad5620479499b9302a2c2df8e6f7e',
-			'client_secret':'b0862bc7285b4e4a838dc7047fc0384a',
-			'grant_type':'authorization_code',
-			'redirect_uri':'http://kumafu.github.io/a-map/',
-			'code':_code
-		},
 		success: function(_json) {
 			console.log(_json);
 		}
